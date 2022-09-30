cfd7673007b661bfc51fa88f48c8950b10df7b1e
SalwinA <salwino@gmail.com>
2022-09-30 12:30:59 +0000

    Initial commit.

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "5aBVpGt8jZtQgn7lP2CfG8ZOujQ";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
